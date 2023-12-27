f40c6a4f7e09331eb07ec7b7131cb2b84ad7f845
Leylaahaciyeva <112489440+Leylaahaciyeva@users.noreply.github.com>
2023-12-27 23:23:42 +0400

    Add files via upload

---
+++ Home-Tasks/Scroll Animation/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+const animationItems = document.querySelectorAll(".animationItem");
+
+const scrollAnimation=()=>{
+    animationItems.forEach((animationItem)=>{
+        const triggerPoint=window.innerHeight*0.7;
+        const{top:distanceFromTop}=animationItem.getBoundingClientRect();
+
+        animationItem.style.transition="all 200ms linear";
+
+        if (distanceFromTop<triggerPoint) {
+            animationItem.style.opacity="1";
+            animationItem.style.transform="translateY(0)";
+        }
+        else{
+            animationItem.style.opacity="0";
+            animationItem.style.transform="translateY(50px)";
+        }
+    });
+};
+scrollAnimation();
+window.addEventListener("scroll",scrollAnimation);
